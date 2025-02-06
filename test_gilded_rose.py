3b309ae54dd6a4d13e9c96c5b7f74c2af2b60f28
Rachel <129236236+Rachelrcx@users.noreply.github.com>
2025-02-05 22:13:40 -0800

    remove old tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -43,23 +43,5 @@ class GildedRoseTest(unittest.TestCase):
             gilded_rose.get_all_items()
 
 
-    # example of test that checks for logical errors
-    def test_sulfuras_should_not_decrease_quality(self):
-        items = [Item("Sulfuras", 5, 80)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        sulfuras_item = items[0]
-        self.assertEqual(80, sulfuras_item.quality)
-        self.assertEqual(4, sulfuras_item.sell_in)
-        self.assertEqual("Sulfuras", sulfuras_item.name)
-
-    # example of test that checks for syntax errors
-    def test_gilded_rose_list_all_items(self):
-        items = [Item("Sulfuras", 5, 80)]
-        gilded_rose = GildedRose(items)
-        all_items = gilded_rose.get_item()
-        self.assertEqual(["Sulfuras"], all_items)
-
-
 if __name__ == '__main__':
     unittest.main()
